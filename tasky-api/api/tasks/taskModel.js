e53789f7e88254de02244b9d193a36ef6cbd9dd7
Jia Yang <20104736@mail.wit.ie>
2023-11-28 14:46:35 +0000

    Mongo lab Error Handling

--- tasky-api/api/tasks/taskModel.js
+++ tasky-api/api/tasks/taskModel.js
@@ -4,14 +4,18 @@ const Schema = mongoose.Schema;
 //构造函数
 
 const TaskSchema = new Schema({
-  title:  String,
-  description:  String ,
-  deadline: Date,
+    title: { type: String, required: true },
+    description: String,
+    deadline: Date,
     done: Boolean,
-    priority: {type: String, enum: ["Low","Medium","High"]},
+    priority: { type: String, enum: ["Low", "Medium", "High"], required: true },
     created_at: Date,
     updated_at: Date
 });
 
 //将架构实例与名为“Task”的数据库集合关联起来,并返回模型对象
 export default mongoose.model('Task', TaskSchema);
+const dateValidator = (date) => {
+    return date > new Date();
+}
+TaskSchema.path("deadline").validate(dateValidator);
